1740ac29b1e4f0fecee2c0c3d4854c97820f2f30
JethroMuluba <jethro.muluba@gmail.com>
2025-03-07 17:53:38 +0100

    Feat : Create Json Data File To Stock All Assets

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,6 +5,7 @@ import Link from "next/link"
 import Image from "next/image"
 import { Mail, MapPin, Phone, Menu, ArrowRight } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import TopHeader from "./topheader"
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
@@ -12,26 +13,7 @@ export default function Header() {
   return (
     <header className="w-full">
       {/* Top Header */}
-      <div className="bg-gradient-to-r from-[#320001] to-[#BD2222] py-2 hidden md:block px-15">
-        <div className="container-custom flex flex-col md:flex-row justify-between items-center gap-2 md:gap-0">
-          <div className="flex items-center gap-2">
-            <Mail className="w-4 h-4 text-white" />
-            <span className="text-xs font-thin tracking-wide  text-white">contact@enywork.com</span>
-          </div>
-
-          <div className="flex items-center gap-2">
-            <MapPin className="w-4 h-4 text-white" />
-            <span className="text-xs tracking-wide font-thin text-white">
-              Ancienne Galerie Présidentielle, 6è niveau, Local 6B8, Kinshasa/Gombe
-            </span>
-          </div>
-
-          <div className="flex items-center gap-2">
-            <Phone className="w-4 h-4 text-white" />
-            <span className="text-xs tracking-wide font-thin text-white">+243 899 467 398</span>
-          </div>
-        </div>
-      </div>
+      <TopHeader />
 
       {/* Main Header */}
       <div className="py-4 shadow-sm">
--- src/components/topheader.tsx
+++ src/components/topheader.tsx
@@ -1,28 +1,31 @@
 'use client'
-import { BsEnvelope } from "react-icons/bs";
-import { HiOutlineMapPin } from "react-icons/hi2";
-import { IoCallOutline } from "react-icons/io5";
+import { Mail, Phone, MapPin } from "lucide-react";
+
 
 
 const TopHeader: React.FC = () => {
 
     return (
-        <div className=" bg-red-600 px-15 flex justify-between text-sm">
-            <div className=" flex items-center gap-2">
-                <BsEnvelope /> 
-                <p className="text-white">contact@enywrk.com</p>
-            </div>
+        <div className="bg-gradient-to-r from-[#320001] to-[#BD2222] py-2 hidden md:block px-15">
+            <div className="container-custom flex flex-col md:flex-row justify-between items-center gap-2 md:gap-0">
+                <div className="flex items-center gap-2">
+                    <Mail className="w-4 h-4 text-white" />
+                    <span className="text-xs font-thin tracking-wide  text-white">contact@enywork.com</span>
+                </div>
 
-            <div className=" flex items-center gap-2">
-                <HiOutlineMapPin />
-                <p className="text-white">Ancienne Galerie Présidentielle, 6è niveau, Local 6B8, Kinshasa/Gombe</p>
-            </div>
+                <div className="flex items-center gap-2">
+                    <MapPin className="w-4 h-4 text-white" />
+                    <span className="text-xs tracking-wide font-thin text-white">
+                    Ancienne Galerie Présidentielle, 6è niveau, Local 6B8, Kinshasa/Gombe
+                    </span>
+                </div>
 
-            <div className=" flex items-center gap-2">
-                <IoCallOutline />
-                <p className="text-white">+243 899 467 398</p>
+                <div className="flex items-center gap-2">
+                    <Phone className="w-4 h-4 text-white" />
+                    <span className="text-xs tracking-wide font-thin text-white">+243 899 467 398</span>
+                </div>
             </div>
-        </div>
+      </div>
     )
 };
 
